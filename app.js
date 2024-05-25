b34a4a3afd899cc224cc39da748371089d1fd534
user <d1204433@o365.fcu.edu.tw>
2024-05-25 14:16:34 +0800

    v1.83

--- app.js
+++ app.js
@@ -3,19 +3,17 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sqlite3 = require('sqlite3').verbose();
-const cors = require('cors'); // Import cors module
+const cors = require('cors');
 
 const app = express();
 
-// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
-// Create database connection
 const dbPath = path.join(__dirname, 'db', 'sqlite.db');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
@@ -25,13 +23,8 @@ const db = new sqlite3.Database(dbPath, (err) => {
     }
 });
 
-// Search endpoint for filtering data between dates
 app.get('/search', (req, res) => {
     const { startDate, endDate } = req.query;
-
-    console.log('Received request to search data between dates:', startDate, 'and', endDate);
-
-    // Query database for data between startDate and endDate
     const query = `SELECT Date, Close, Volume FROM BTCUSD WHERE Date BETWEEN ? AND ?`;
     db.all(query, [startDate, endDate], (err, rows) => {
         if (err) {
@@ -49,11 +42,8 @@ app.get('/search', (req, res) => {
     });
 });
 
-// Endpoint for inserting data into BTCUSD table
 app.post('/insert', (req, res) => {
     const { date, open, high, low, close, adj_close, volume } = req.body;
-
-    // Insert data into BTCUSD table
     const insertSQL = `INSERT INTO BTCUSD (Date, Open, High, Low, Close, Adj_Close, Volume) VALUES (?, ?, ?, ?, ?, ?, ?)`;
     db.run(insertSQL, [date, open, high, low, close, adj_close, volume], (err) => {
         if (err) {
@@ -66,7 +56,6 @@ app.post('/insert', (req, res) => {
     });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
