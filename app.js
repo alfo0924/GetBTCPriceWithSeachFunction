6c4843c42aebcd4b9eafa3fd2929d1e4f3c69d16
user <d1204433@o365.fcu.edu.tw>
2024-05-25 09:11:52 +0800

    v1.37

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const sqlite3 = require('sqlite3').verbose(); // Import SQLite module
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
